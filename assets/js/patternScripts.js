b3117675c728eabf5ca83959b4d5363ed6abbb85
Jack Mai <mymjack@hotmail.com>
2016-12-01 06:02:09 -0500

    Adding pattern details page
    Please bear with the quality. Time insufficient.

---
+++ assets/js/patternScripts.js
@@ -0,0 +1,37 @@
+$( document ).ready(function() {
+	superInit();
+	serversays();
+});
+
+function serversays() {
+	if ($("#serversays").length!=0) {
+		var data = JSON.parse($("#serversays").html());
+		$("#serversays").remove();
+
+		if (data.username && data.verbose) {
+			$($("#nav-pbrief").find("span")[0]).html(data.username);
+			$($("#nav-pbrief").find("span")[1]).html(data.verbose);
+		}
+		if (data.pattern) {
+			var p = data.pattern;
+			$("#desc").find("img").attr("src", p.src);
+			$("#desc").find("div.pattern-desc").find("h4").html(p.name)
+				.next().html("Author: "+p.author).next().html("Posted: "+p.time);
+			$("#try").attr("href", "./game?pid="+p.pid);
+			$("#like").click(function(){like(p.pid);})
+				.find("span").html(p.liked?"Liked ":"like "+p.likes);
+		} 
+	}
+}
+
+function like(pid) {
+		$.post({
+        url: "/toggleLike",
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify( { pid: pid } ),
+        success: function(r) {
+        	$("#like").find("span").html(r.liked?"Liked ":"like "+r.likes);
+        }
+    });
+}
